482c1ac651bb2200b9759cf7667ff105533398bb
z-kk <z-kk@outlook.jp>
2020-12-10 09:56:31 +0900

    parseMultiPartData関数作成

--- src/jesterUtils.nim
+++ src/jesterUtils.nim
@@ -1,6 +1,10 @@
 import
   json, tables, strutils
 
+type
+  HttpDataInfo* = object
+    name*, fileName*, contType*, data*: string
+
 proc parseNode(params: Table[string, string], keyword: string): JsonNode =
   result = %*{}
   for key, val in params:
@@ -16,8 +20,8 @@ proc parseNode(params: Table[string, string], keyword: string): JsonNode =
         continue
       result[k[k.rfind('[')+1..^2]] = params.parseNode(k)
 
-# Jesterのrequest.paramsをJsonに変換
 proc parseJson*(params: Table[string, string]): JsonNode =
+  ## Jesterのrequest.paramsをJsonに変換
   result = %*{}
   for key, val in params:
     var s = key.find('[')
@@ -30,3 +34,69 @@ proc parseJson*(params: Table[string, string]): JsonNode =
       if result.hasKey(k):
         continue
       result[k] = params.parseNode(k)
+
+proc parseMultiPartData*(body: string): seq[HttpDataInfo] =
+  ## multipart/form-dataで送信されたデータを解析
+  let lineList = body.split("\r\n")
+  if lineList.len < 3:
+    return
+
+  let separator = lineList[0]
+  var isData = false
+  for line in lineList:
+    if not isData or line.startsWith(separator) or result[^1].contType == "":
+      echo line
+    else:
+      echo "--data--"
+
+    if line == separator:
+      result.add(HttpDataInfo())
+      isData = false
+    elif line.startsWith(separator):
+      return
+    elif line == "":
+      isData = true
+    elif not isData:
+      var i = line.find("; name=\"")
+      if i > -1:
+        # nameを設定する
+        let str = line[i + 8 .. ^1]
+        i = str.find('"')
+
+        while i > 0 and str[i - 1] == '\\':  # エスケープ文字を考慮
+          i.inc(str[i + 1 .. ^1].find('"'))
+
+        if i == str.len - 1 or str[i + 1] == ';':
+          result[^1].name = str[0 ..< i]
+        else:
+          raiseAssert("name取得失敗")
+
+      i = line.find("; filename=\"")
+      if i > -1:
+        # fileNameを設定する
+        let str = line[i + 12 .. ^1]
+        i = str.find('"')
+
+        while i > 0 and str[i - 1] == '\\':  # エスケープ文字を考慮
+          i.inc(str[i + 1 .. ^1].find('"'))
+
+        if i == str.len - 1 or str[i + 1] == ';':
+          result[^1].fileName = str[0 ..< i]
+        else:
+          raiseAssert("filename取得失敗")
+
+      i = line.find("Content-Type: ")
+      if i > -1:
+        # contTypeを設定する
+        let str = line[i + 14 .. ^1]
+        i = str.find(';')
+        result[^1].contType =
+          if i < 0:
+            str
+          else:
+            str[0 ..< i]
+    else:
+      if result[^1].data != "":
+        result[^1].data &= "\r\n" & line
+      else:
+        result[^1].data = line
--- tests/test1.nim
+++ tests/test1.nim
@@ -5,3 +5,10 @@ test "parseJson":
   var params: Table[string, string]
   params = {"hoge[fuga][foo][boo]": "true", "152005[eres]": "true", "hoge[hoge]": "true", "hoge[fuga][boo]": "false", "152005[ares]": "true"}.toTable
   check params.parseJson["hoge"]["fuga"]["foo"]["boo"].getStr == "true"
+
+test "parseMultiPartData":
+  let
+    data = readFile("tests/multipartdata")
+    datas = data.parseMultiPartData
+
+  check datas[0].name & datas[1].contType & datas[2].fileName & datas[3].data == "aimage/bmpc.jpghogedata"
